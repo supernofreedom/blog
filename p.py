472124a6804b9dc8797e8754f81ba09ec3c4ef43
xkguo <xkguo@nurlink.com>
2019-01-15 09:08:03 +0800

    hello

--- p.py
+++ p.py
@@ -1,8 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-t=np.arange(0,5,0.2)
-x=np.linspace(0,2*np.pi, 100)
-y=np.sin(x)
-plt.plot(x, y, 'r--', x, (x**2)/100, 'bs')
-plt.show()
+def demo(N):
+    #t=np.arange(0,5,0.2)
+    x=np.linspace(0, 8*np.pi, N)
+    y=np.sin(x)
+    z = np.fft.fft(y)
+    w = np.fft.ifft(z)
+    plt.plot( x, z, 'b--', x, w, 'g--' )
+    plt.show()
+
+if __name__=='__main__':
+    demo(2048)
